1fe71339a4344c18604b3094d3a675f183562764
Bentley, Matt (UK - London) <matthew_j_bentley@hotmail.com>
2020-08-24 14:17:46 +0100

    added test client

--- AspNetCore.ServiceBenchmark.TestClient/Worker.cs
+++ AspNetCore.ServiceBenchmark.TestClient/Worker.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using AspNetCore.ServiceBenchmark.Grpc.Services;
 using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,22 +13,51 @@ namespace AspNetCore.ServiceBenchmark.TestClient
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration Configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             // The port number(5001) must match the port of the gRPC server.
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new TestDataService.TestDataServiceClient(channel);
-            var reply = await client.GetByIndexAsync(
-                              new IndexMessage { Index = 1 });
-            Console.WriteLine("Generated: " + reply.Id);
+            try
+            {
+                await TestLoop(1000);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex}");
+            }
+                
             Console.WriteLine("Press any key to exit...");
+        }
+
+        private async Task TestLoop(int count)
+        {
+            using (var channel = GrpcChannel.ForAddress(Configuration["Service:Url"]))
+            {
+                var client = new TestDataService.TestDataServiceClient(channel);
+
+                var sw = new Stopwatch();
+                sw.Start();
+
+                for(int i = 0; i < count; i++)
+                {
+                    var reply = await client.GetByIndexAsync(new IndexMessage { Index = i });
+                    if (i % 100 == 0)
+                    {
+                        _logger.LogInformation($"Processed {i}/{count}");
+                    }
+                }
+                sw.Stop();
+
+                _logger.LogInformation($"Processed {count}/{count} in {sw.ElapsedMilliseconds}ms");
+            }
         }
     }
 }
